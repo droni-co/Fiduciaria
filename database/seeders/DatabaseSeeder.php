86202a42b3c89611c995a34681dfe11428eef9f2
Gustavo Barragan <dev@droni.co>
2024-09-18 15:41:54 -0500

    seeder database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,16 +8,17 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
-    {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
-    }
+  /**
+   * Seed the application's database.
+   */
+  public function run(): void
+  {
+    User::factory()->create([
+      'name' => 'Kalvin Manson',
+      'email' => 'kalvinmanson@gmail.com',
+      'password' => bcrypt('Kalvinista01'),
+      'docType' => 'CC',
+      'docNumber' => '1234567890'
+    ]);
+  }
 }
