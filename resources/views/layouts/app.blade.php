115101bea1249d80ec9e0e53e4a5ca738ff9fe99
Gustavo Barragan <dev@droni.co>
2024-09-18 17:55:05 -0500

    admin menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,48 +28,51 @@
                 </button>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
-                      <li class="nav-item">
-                        <a class="nav-link" href="{{ route('balances.index') }}">{{ __('Saldos y movimientos') }}</a>
-                      </li>
-                    </ul>
+                  <!-- Left Side Of Navbar -->
+                  <ul class="navbar-nav me-auto">
+                    <li class="nav-item">
+                      <a class="nav-link" href="{{ route('balances.index') }}">{{ __('Saldos y movimientos') }}</a>
+                    </li>
+                  </ul>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ms-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
+                  <!-- Right Side Of Navbar -->
+                  <ul class="navbar-nav ms-auto">
+                    <li class="nav-item dropdown">
+                      <a
+                        id="navbarDropdown1"
+                        class="nav-link dropdown-toggle"
+                        href="#"
+                        role="button"
+                        data-bs-toggle="dropdown"
+                        aria-haspopup="true"
+                        aria-expanded="false" v-pre>
+                        Admin
+                      </a>
 
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
+                      <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{ route('admin.trusts.index') }}">
+                          Trusts
+                        </a>
+                      </div>
+                    </li>
+                    <li class="nav-item dropdown">
+                      <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        {{ Auth::user()->name }}
+                      </a>
 
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
+                      <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{ route('logout') }}"
+                          onclick="event.preventDefault();
+                                        document.getElementById('logout-form').submit();">
+                          {{ __('Logout') }}
+                        </a>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                          @csrf
+                        </form>
+                      </div>
+                    </li>
+                  </ul>
                 </div>
             </div>
         </nav>
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\BalanceController;
+//Admin controllers
+use App\Http\Controllers\Admin\TrustController as AdminTrustController;
 use Illuminate\Support\Facades\Auth;
 
 Auth::routes();
@@ -10,4 +12,15 @@ Auth::routes();
 Route::middleware(['auth'])->group(function () {
   Route::get('/', [DashboardController::class, 'home'])->name('home');
   Route::get('/balances', [BalanceController::class, 'index'])->name('balances.index');
+
+  
 });
+
+Route::middleware(['auth'])
+  ->prefix('admin')
+  ->name('admin.')
+  ->group(function () {
+  // Admin
+  Route::apiResource('trusts', AdminTrustController::class);
+});
+
